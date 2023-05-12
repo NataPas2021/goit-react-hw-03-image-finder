6bde4efd8e209ce824ef6fed8fd8b7648dcb92b0
NataPas2021 <natapas13@gmail.com>
2023-05-12 23:37:10 +0300

    api

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiKey = '28539221-d5e0309a6fde535568a0abe02';
+
 const fetchImagesWithQuery = async searchQuery => {
-    const response = axios.get(`/search?query=${searchQuery}`);
+    const response = axios.get(`https://pixabay.com/api/?q=${searchQuery}&page=1&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=12`);
     return (await response).data.hits;
 }
 
